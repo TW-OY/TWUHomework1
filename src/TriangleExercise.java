0e53b10e2b586a03e3abf0c7517be0dd82282954
Yingjie Ouyang <ouyang_yingjie@outlook.com>
2016-07-25 20:03:21 +0800

    fea:finish task2

--- src/TriangleExercise.java
+++ src/TriangleExercise.java
@@ -1,5 +1,14 @@
 public class TriangleExercise {
     public static void main(String[] args) {
-        System.out.println("*");
+        printNstar(8);
+    }
+
+    public static void printNstar(int n) {
+        for(int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
     }
 }
+
+
