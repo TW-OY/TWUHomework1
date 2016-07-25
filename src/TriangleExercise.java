e063e2b95d601d69154834cf968c2721ddc2f3c6
Yingjie Ouyang <ouyang_yingjie@outlook.com>
2016-07-25 20:04:57 +0800

    fea:finish task3

--- src/TriangleExercise.java
+++ src/TriangleExercise.java
@@ -1,13 +1,19 @@
 public class TriangleExercise {
     public static void main(String[] args) {
-        printNstar(8);
+        printNstarVertical(8);
     }
 
-    public static void printNstar(int n) {
+    public static void printNstarHorizontal(int n) {
         for(int i = 0; i < n; i++) {
             System.out.print("*");
         }
         System.out.print("\n");
+    }
+
+    public static void printNstarVertical(int n) {
+        for(int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
     }
 }
 
