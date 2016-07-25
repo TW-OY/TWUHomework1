78763653971ac4e3e1efeb4f7d51968f4f9ed80a
Yingjie Ouyang <ouyang_yingjie@outlook.com>
2016-07-25 21:12:05 +0800

    fea;finish task5

--- src/TriangleExercise.java
+++ src/TriangleExercise.java
@@ -1,27 +1,38 @@
 public class TriangleExercise {
     public static void main(String[] args) {
-        printTriangle(3);
+        printCenteredTriangle(4);
     }
 
-    public static void printNstarHorizontal(int n) {
-        for(int i = 0; i < n; i++) {
-            System.out.print("*");
+    public static void printNCharHorizontal(int n, String s) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(s);
         }
-        System.out.print("\n");
+        System.out.print('\n');
     }
 
     public static void printNstarVertical(int n) {
-        for(int i = 0; i < n; i++) {
-            System.out.println("*");
+        for (int i = 0; i < n; i++) {
+            System.out.println('*');
         }
     }
 
     public static void printTriangle(int n) {
-        for(int i = 1; i <= n; i++) {
-            printNstarHorizontal(i);
+        for (int i = 1; i <= n; i++) {
+            printNCharHorizontal(i, "*");
         }
     }
 
+    public static void printCenteredTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 2 - 1; j++) {
+                if(j >= ((n * 2) - 1) / 2 - i && j <= ((n * 2) - 1) / 2 + i ){
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
 }
-
 
