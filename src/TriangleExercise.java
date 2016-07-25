06bbde773c5917f10eff87ff8b12c2040bfda68c
Yingjie Ouyang <ouyang_yingjie@outlook.com>
2016-07-25 21:31:29 +0800

    fea:finish task7

--- src/TriangleExercise.java
+++ src/TriangleExercise.java
@@ -1,6 +1,6 @@
 public class TriangleExercise {
     public static void main(String[] args) {
-        printDiamond(4);
+        printDiamondName(4, "bill");
     }
 
     public static void printNCharHorizontal(int n, String s) {
@@ -54,9 +54,32 @@ public class TriangleExercise {
         System.out.print(centeredTriangle.toString());
     }
 
+    public static void printReversedCenteredTriangleNameDiamond(int n) {
+        StringBuilder centeredTriangle = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 2 - 1; j++) {
+                if(j >= ((n * 2) - 1) / 2 - i && j <= ((n * 2) - 1) / 2 + i ){
+                    centeredTriangle.append("*");
+                } else{
+                    centeredTriangle.append(" ");
+                }
+            }
+            centeredTriangle.append("\n");
+        }
+        centeredTriangle.reverse();
+        centeredTriangle.replace(0, 1, "");
+        System.out.print(centeredTriangle.toString());
+    }
+
     public static void printDiamond(int n){
         printCenteredTriangle(n);
         printReversedCenteredTriangle(n);
+    }
+
+    public static void printDiamondName(int n, String name) {
+        printCenteredTriangle(n - 1);
+        System.out.println(name);
+        printReversedCenteredTriangleNameDiamond(n - 1);
     }
 
 }
