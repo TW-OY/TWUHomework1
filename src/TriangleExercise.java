80cb9982226f4c4f4b104a07d113ed2d14ad6c2e
Yingjie Ouyang <ouyang_yingjie@outlook.com>
2016-07-25 21:26:48 +0800

    fea:finish task6

--- src/TriangleExercise.java
+++ src/TriangleExercise.java
@@ -1,6 +1,6 @@
 public class TriangleExercise {
     public static void main(String[] args) {
-        printCenteredTriangle(4);
+        printDiamond(4);
     }
 
     public static void printNCharHorizontal(int n, String s) {
@@ -23,16 +23,41 @@ public class TriangleExercise {
     }
 
     public static void printCenteredTriangle(int n) {
+        StringBuilder centeredTriangle = new StringBuilder();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n * 2 - 1; j++) {
                 if(j >= ((n * 2) - 1) / 2 - i && j <= ((n * 2) - 1) / 2 + i ){
-                    System.out.print("*");
+                    centeredTriangle.append("*");
                 } else{
-                    System.out.print(" ");
+                    centeredTriangle.append(" ");
                 }
             }
-            System.out.print("\n");
+            centeredTriangle.append("\n");
         }
+        System.out.print(centeredTriangle.toString());
     }
+
+    public static void printReversedCenteredTriangle(int n) {
+        StringBuilder centeredTriangle = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 2 - 1; j++) {
+                if(j >= ((n * 2) - 1) / 2 - i && j <= ((n * 2) - 1) / 2 + i ){
+                    centeredTriangle.append("*");
+                } else{
+                    centeredTriangle.append(" ");
+                }
+            }
+            centeredTriangle.append("\n");
+        }
+        centeredTriangle.reverse();
+        centeredTriangle.replace(0, n * 2 + 1, "");
+        System.out.print(centeredTriangle.toString());
+    }
+
+    public static void printDiamond(int n){
+        printCenteredTriangle(n);
+        printReversedCenteredTriangle(n);
+    }
+
 }
 
